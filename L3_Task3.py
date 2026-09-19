a5b03423c3efb194718c4fb4751c4ee719b6e989
Nishno <gnishnianidze96@gmail.com>
2026-09-19 15:27:09 +0400

    add homework3

---
+++ L3_Task3.py
@@ -0,0 +1,5 @@
+user_name = input("Enter your name:")
+user_comp_name = input("Enter your company's name:")
+
+print(f"Hello {user_name.strip().capitalize()}, your workspace is {user_comp_name.strip().capitalize()}")
+
